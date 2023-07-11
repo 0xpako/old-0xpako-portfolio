a42de5c6412436d5f0dbf3561a723a1566d7c0a8
Mateusz <77123763+0xpako@users.noreply.github.com>
2023-07-11 13:30:34 +0200

    Update Environment.js

--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -31,7 +31,7 @@ export default class Environment {
     }
 
     setSunlight() {
-        this.sunLight = new THREE.DirectionalLight("#FFD580", 3);
+        this.sunLight = new THREE.DirectionalLight("#ff8080", 3);
         this.sunLight.castShadow = true;
         this.sunLight.shadow.camera.far = 20;
         this.sunLight.shadow.mapSize.set(2048, 2048);
@@ -42,7 +42,7 @@ export default class Environment {
         this.sunLight.position.set(-1.5, 7, 3);
         this.scene.add(this.sunLight);
 
-        this.ambientLight = new THREE.AmbientLight("#FFD580", 1);
+        this.ambientLight = new THREE.AmbientLight("#ff8080", 1);
         this.scene.add(this.ambientLight);
     }
 
